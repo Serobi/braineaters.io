2c872bb0f7fd7eec8e2d54a4d2fb50ea579e6fae
Serobi <paul.nelaton@gmail.com>
2026-01-12 16:03:49 -0300

    update

--- app/components/ImageCarousel.jsx
+++ app/components/ImageCarousel.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import styles from "../styles/carousel.module.css";
 
 export default function ImageCarousel({ images }) {
